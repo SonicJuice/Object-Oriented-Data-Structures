17d0ae1f75c23dbd86b20157cd9834878450334c
LiamP921 <125910815+LiamP921@users.noreply.github.com>
2023-04-24 16:40:14 +0100

    Update Linear Queue Object.py

--- Queues/Linear Queue Object.py
+++ Queues/Linear Queue Object.py
@@ -1,15 +1,18 @@
 
 
 class LinearQueue(object):
-
-    def __init__(self,qSize):
-        self.__q = [None] * qSize
+    """ constructor method initializes the queue with a given size qSize. It creates a list of Nones of 'q_size' to represent the queue, 
+    initialises the front and rear pointers, the size of the queue, and its maximum size.  """
+    def __init__(self,q_size):
+        self.__q = [None] * q_size
         self.__front = 0
         self.__rear = -1
         self.__size = 0
-        self.__max_size = qSize
+        self.__max_size = q_size
 
     def enQueue(self,item):
+        """ adds an element item to the rear, first checking if the queue is full. Otherwise, it increments the rear pointer, 
+        assigns the item to the rear of the queue, increments the size of the queue, and returns True. """
         if self.isFull():
             return False
         else:
@@ -19,6 +22,8 @@ class LinearQueue(object):
             return True
 
     def deQueue(self):
+        """ removes and returns the element from the front, first checking if the queue is empty. Otherwise, it decrements the size of the queue, 
+        retrieves the element from the front of the queue, increments the front pointer, and returns the retrieved element. """
         if self.isEmpty():
             return None
         else:
