fb2252ae4d35f9257d916ea22c6fc317007cd8eb
LiamP921 <125910815+LiamP921@users.noreply.github.com>
2023-04-30 21:10:34 +0100

    Update Linear Queue Object.py

--- Queues/Linear Queue Object.py
+++ Queues/Linear Queue Object.py
@@ -3,29 +3,28 @@
 class LinearQueue(object):
     """ constructor method initializes the queue with a given size qSize. It creates a list of Nones of 'q_size' to represent the queue, 
     initialises the front and rear pointers, the size of the queue, and its maximum size.  """
-    def __init__(self,q_size):
+    def __init__(self, q_size):
         self.__q = [None] * q_size
         self.__front = 0
         self.__rear = -1
         self.__size = 0
         self.__max_size = q_size
 
-    def enQueue(self,item):
+    def enQueue(self, item):
         """ adds an element item to the rear, first checking if the queue is full. Otherwise, it increments the rear pointer, 
-        assigns the item to the rear of the queue, increments the size of the queue, and returns True. """
+        assigns the item to the rear of the queue and increments the size of the queue, and returns True. """
         if self.isFull():
             raise ValueError("Queue is full.")
         else:
             self.__rear += 1
             self.__q[self.__rear] = item
             self.__size += 1
-            return True
 
     def deQueue(self):
         """ removes and returns the element from the front, first checking if the queue is empty. Otherwise, it decrements the size of the queue, 
         retrieves the element from the front of the queue, increments the front pointer, and returns the retrieved element. """
         if self.isEmpty():
-            raise ValueError("Queue is empty. ")
+            raise ValueError("Queue is empty.")
         else:
             self.__size -= 1
             item = self.__q[self.__front]
@@ -33,10 +32,10 @@ class LinearQueue(object):
             return item
 
     def isFull(self):
-        return self.__rear == self.__max_size - 1
+        return self.__size == self.__max_size
 
     def isEmpty(self):
         return self.__size == 0
 
-    def __str__(self):
-        return " ".join[(str(item) for item in self.__q[self.__front:self.__rear + 1])]
+    def showQueue(self):
+        return " ".join(str(item) for item in self.__q[self.__front:self.__rear + 1])
