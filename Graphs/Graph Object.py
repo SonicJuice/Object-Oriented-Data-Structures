ffa3a54702d17e309466da31b95876be30479cfc
LiamP921 <125910815+LiamP921@users.noreply.github.com>
2023-04-30 19:05:55 +0100

    Update Graph Object.py

--- Graphs/Graph Object.py
+++ Graphs/Graph Object.py
@@ -29,12 +29,14 @@ class Node:
 
 
 class Graph:
-    """ for reference, a graph is a set of nodes connected by edges, which may be directed/undirected and weighted/unweighted. Here, an adjacency list is utilised for the space-efficient implementation of a sparsely connected graph. """
+    """ for reference, a graph is a set of nodes connected by edges, which may be directed/undirected and weighted/unweighted. 
+    Here, an adjacency list is utilised for the space-efficient implementation of a sparsely connected graph. """
     def __init__(self):
         self.__nodes = {}
 
     def addNode(self, new_node):
-        " if the new node isn't yet in the AL, add the new node as a key in '__nodes' w/ an empty dictionary as its value, representing no outgoing edges from the new node. """
+        """ if the new node isn't yet in the AL, add the new node as a key in '__nodes' w/ an empty dictionary as its value, 
+        representing no outgoing edges from the new node. """
         if new_node not in self.__nodes:
             self.__nodes[new_node] = Node(new_node)
         else:
@@ -63,7 +65,8 @@ class Graph:
             self.nodes[origin] = Node(origin)
         if destination not in self.__nodes:
             self.__nodes[destination] = Node(destination)
-        """ if either node doesn't exist, add it to the AL as empty dictionaries. If the weight is numeric and there's no pre-existing edge, add a new key-value pair to '__nodes'.  """
+        """ if either node doesn't exist, add it to the AL as empty dictionaries. If the weight is numeric and there's no 
+        pre-existing edge, add a new key-value pair to '__nodes'.  """
         if not isinstance(weight, (int, float)):
             raise ValueError("Non-numeric weight.")
         self.__nodes[origin].createEdge(destination, weight)
@@ -83,7 +86,9 @@ class Graph:
         return self.__nodes[origin].getConnection(destination)
 
     def depthFirst(self, node, visited = None):
-        """ utilises recursive calls to traverse a graph from the current node, going as far down as possible before backtracking. The given node is added to 'visited'; if the neighboring node hasn't been visited, pass it as an argument into the recursion. This continues until all nodes reachable from the starting node have been visited. """
+        """ utilises recursive calls to traverse a graph from the current node, going as far down as possible before backtracking. The given node 
+        is added to 'visited'; if the neighboring node hasn't been visited, pass it as an argument into the recursion. This continues until 
+        all nodes reachable from the starting node have been visited. """
         if node not in self.__nodes:
             raise ValueError("Node doesn't exist.")
 
@@ -97,7 +102,9 @@ class Graph:
                 self.depthFirst(neighbour, visited)
 
     def breadthFirst(self, node):
-        """ utilises a queue to explore nodes at the same depth before ascending. The given node is added to 'queue' and 'visited', before a loop is entered until 'queue' is empty. In each iteration, dequeue the front node and explore its neighbours; for each, if it hasn't been visited, enqueue it to the back and add it to 'visited'. This continues until all nodes reachable from the starting node have been visited. """
+        """ utilises a queue to explore nodes at the same depth before ascending. The given node is added to 'queue' and 'visited', before a loop 
+        is entered until 'queue' is empty. In each iteration, dequeue the front node and explore its neighbours; for each, if it hasn't been visited, 
+        enqueue it to the back and add it to 'visited'. This continues until all nodes reachable from the starting node have been visited. """
         if node not in self.__nodes:
             raise ValueError("Node doesn't exist.")
 
@@ -116,7 +123,8 @@ class Graph:
                     visited.add(neighbour)
 
     def dijkstrasShortestPath(self, node):
-        """ for this method, add edges in the form ("A", "B"), ("B", "A"). Here, a dictionary is initialised with the source as the key and its distance from itself as 0; all other node distances are set to infinity. """
+        """ for this method, add edges in the form ("A", "B"), ("B", "A"). Here, a dictionary is initialised with the 
+        source as the key and its distance from itself as 0; all other node distances are set to infinity. """
         if node not in self.__nodes:
             raise ValueError("Node doesn't exist.")
             
@@ -129,15 +137,18 @@ class Graph:
         visited = {}
 
         while pq:
-            """ a heap is used to implement a priority queue; 'heappop' removes and returns the smallest element, whilst the order is adjusted to maintain the order properties (see 'Priority Queue Object.py'). """
+            """ a heap is used to implement a priority queue; 'heappop' removes and returns the smallest element, whilst the order is adjusted 
+            to maintain the order properties (see 'Priority Queue Object.py'). """
             distance, current_node = heappop(pq)
             if current_node in visited:
                 continue
             visited[current_node] = True
 
-            """ update the distances to the neighboring nodes of 'current_node' if a shorter path is found. For each neighboring node, calculate the distance to 'neighbour' through 'current_node'. """
+            """ update the distances to the neighboring nodes of 'current_node' if a shorter path is found. For each neighboring node, 
+            calculate the distance to 'neighbour' through 'current_node'. """
             for neighbour, weight in self.__nodes[current_node].edges.items():
-                """ achieved by adding the distance from 'current_node' to 'neighbour' to the distance from the source node to 'current_node'. If this is < the current distance to neighbour in 'distances', update the distance and push (new_distance, neighbour). """
+                """ achieved by adding the distance from 'current_node' to 'neighbour' to the distance from the source node to 'current_node'. 
+                If this is < the current distance to neighbour in 'distances', update the distance and push (new_distance, neighbour). """
                 tentative_distance = distances[current_node] + weight
                 if tentative_distance < distances[neighbour]:
                     distances[neighbour] = tentative_distance
