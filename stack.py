a2d03a529c2f47975c90eaa713ba89e2a8510f68
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-20 10:28:46 +0100

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,13 +1,13 @@
 
 
 class Stack(object):
-
-    def __init__(self, max_size, iterable = False):
-        self.__stack = []
-        self.__size = 0
-        self.__max_size = max_size
-        """ constructor method may take the argument 'iterable', which specifies whether the stack should be initialised with items from an iterable.  
-        '__stack' is then intialised to represent the stack, as are the initial size '__size', and the maximum size, 'self.__max_size. """
+    def __init__(self, max_size, iterable=False):
+        self.stack = []
+        self.size = 0
+        self.max_size = max_size
+        """ constructor method may take iterable, which specifies whether the stack should be initialised with items 
+        from an iterable. __stack is then intialised to represent the stack, as are the initial size __size, and the maximum 
+        size, self.max_size. """
         if iterable:
             for item in iterable:
                 self.push(item)
@@ -15,9 +15,9 @@ class Stack(object):
     def push(self,item):
         """ adds a new item to the top of stack; returns False if stack is full. """
         if self.isFull() == False:
-            """ '.insert' places a specified element at a specified list index. """
-            self.__stack.insert(0, item)
-            self.__size += 1
+            """ .insert places a specified element at a specified list index. """
+            self.stack.insert(0, item)
+            self.size += 1
             return True
         else:
             return False
@@ -25,32 +25,32 @@ class Stack(object):
     def pop(self):
         """ returns top item from the stack. """
         if self.isEmpty() == False:
-            self.__size -= 1
-            """ '.pop()' removes an item from a list according to its index value.  """
-            return self.__stack.pop(0)
+            self.size -= 1
+            """ .pop() removes an item from a list according to its index value.  """
+            return self.stack.pop(0)
         else:
             raise ValueError("Stack is empty.")
 
     def peek(self):
         """ returns the top item without removing it. """
-        if self.isEmpty() == False:
-            self.__size -= 1
-            return self.__stack[0]
+        if self.is_empty() == False:
+            self.size -= 1
+            return self.stack[0]
         else:
-            raise ValueError("Stack is empty")
+            raise ValueError("Stack is empty.")
 
     def size(self):
         """ returns no. of items in stack. """
-        return self.__size
+        return self.size
 
-    def isFull(self):
-        return self.__size == self.__max_size
+    def is_full(self):
+        return self.size == self.max_size
 
-    def isEmpty(self):
-        return self.__size == 0
+    def is_empty(self):
+        return self.size == 0
   
-    def showStack(self):
+    def show_stack(self):
         output = ""
-        for item in self.__stack:
+        for item in self.stack:
             output += str(item) + "\n"
         return output
