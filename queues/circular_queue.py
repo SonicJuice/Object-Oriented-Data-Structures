0081f008c8f5da936c1d54a6ea7820633cadb703
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-20 10:45:42 +0100

    Update circular_queue.py

--- queues/circular_queue.py
+++ queues/circular_queue.py
@@ -1,37 +1,37 @@
 
  
 class CircularQueue(object):
-    def __init__(self,qSize):
-        self.__q = [None] * qSize
-        self.__front = 0
-        self.__rear = -1
-        self.__size = 0
-        self.__max_size = qSize
+    def __init__(self, qSize):
+        self.q = [None] * qSize
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+        self.max_size = qSize
 
-    def enQueue(self,item):
-        if self.isFull():
+    def enqueue(self, item):
+        if self.is_full():
             raise ValueError("Queue is full.")
         else:
             """ modulo operator allows pointers to wrap around to the beginning of the array once they reach the end. """
-            self.__rear = (self.__rear + 1) % self.__max_size
-            self.__q[self.__rear] = item
-            self.__size += 1
+            self.rear = (self.rear + 1) % self.max_size
+            self.q[self.rear] = item
+            self.size += 1
             return True
 
-    def deQueue(self):
-        if self.isEmpty():
+    def dequeue(self):
+        if self.is_empty():
             raise ValueError("Queue is empty.")
         else:
-            self.__size -= 1
-            item = self.__q[self.__front]
-            self.__front = (self.__front + 1) % self.__max_size
+            self.size -= 1
+            item = self.q[self.front]
+            self.front = (self.front + 1) % self.max_size
             return item
 
-    def isFull(self):
-        return self.__size == self.__max_size
+    def is_full(self):
+        return self.size == self.max_size
     
-    def isEmpty(self):
-        return self.__size == 0
+    def is_empty(self):
+        return self.size == 0
 
-    def showQueue(self):
-        return " ".join(str(item) for item in self.__q[self.__front:self.__rear + 1])
+    def show_queue(self):
+        return " ".join(str(item) for item in self.q[self.front:self.rear + 1])
