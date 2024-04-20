20a346dab2808b9e3ebde768eddf68c7de7bf0c6
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-20 10:42:23 +0100

    Update priority_queue.py

--- queues/priority_queue.py
+++ queues/priority_queue.py
@@ -2,21 +2,22 @@
 
 class PriorityQueue(object):
     def __init__(self, q_size):
-        self.__q = []
-        self.__size = 0
-        self.__max_size = q_size
+        self.q = []
+        self.size = 0
+        self.max_size = q_size
 
     def insert(self, item, priority):
-        if self.isFull():
+        if self.is_full():
             raise ValueError("Queue is full.")
         else:
-            self.__size += 1
-            i = self.__size - 1
-            self.__q.append((item, priority))
-            """ append a tuple containing the item and its priority, then perform a heapify operation by swapping the new item w/ its parent 
-            until the parent has a higher priority, or the item becomes the root. This ensures that the highest priority item is always at the root. """
-            while i > 0 and priority <= self.__q[(i - 1) // 2][1]:
-                self.__q[i], self.__q[(i - 1) // 2] = self.__q[(i - 1) // 2], self.__q[i]
+            self.size += 1
+            i = self.size - 1
+            self.q.append((item, priority))
+            """ append a tuple containing the item and its priority, then perform a heapify operation by swapping the new item w/ 
+            its parent until the parent has a higher priority, or the item becomes the root. This ensures that the highest priority 
+            item is always at the root. """
+            while i > 0 and priority <= self.q[(i - 1) // 2][1]:
+                self.q[i], self.q[(i - 1) // 2] = self.q[(i - 1) // 2], self.q[i]
                 """ represent parent index. """
                 i = (i - 1) // 2
 
@@ -24,56 +25,57 @@ class PriorityQueue(object):
         if self.isEmpty():
             raise ValueError("Queue is empty.")
         else:
-            item, priority = self.__q[0]
-            self.__q[0] = self.__q[-1]
-            self.__q.pop()
-            self.__size -= 1
+            item, priority = self.q[0]
+            self.q[0] = self.q[-1]
+            self.q.pop()
+            self.size -= 1
             i = 0
 
-            """ store the item and its priority from the root, replace the root with the last item in the heap, and remove the last item from the heap. 
-            Then perform a heapify operation by swapping the root w/ its smallest child until the root has a lower priority than its children, or it becomes a leaf node. """
+            """ store the item and its priority from the root, replace the root with the last item in the heap, and remove the 
+            last item from the heap. Then perform a heapify operation by swapping the root w/ its smallest child until the root 
+            has a lower priority than its children, or it becomes a leaf node. """
             while i >= 0:
                 """ represents child indexes. """
                 left = 2 * i + 1
                 right = 2 * i + 2
                 smallest = i
-                if left < self.__size and self.__q[left][1] <= self.__q[smallest][1]:
+                if left < self.size and self.q[left][1] <= self.q[smallest][1]:
                     smallest = left
-                if right < self.__size and self.__q[right][1] <= self.__q[smallest][1]:
+                if right < self.size and self.q[right][1] <= self.q[smallest][1]:
                     smallest = right
                 if smallest == i:
                     i = -1
                 else:
-                    self.__q[i], self.__q[smallest] = self.__q[smallest], self.__q[i]
+                    self.q[i], self.q[smallest] = self.q[smallest], self.q[i]
                     i = smallest
             return item, priority
 
-    def alterPriority(self, item, new_priority):
+    def alter_priority(self, item, new_priority):
         if self.isEmpty():
             raise ValueError("Queue is empty.")
         found = False
         i = 0
-        while i < self.__size and not found:
-            if self.__q[i][0] == item:
+        while i < self.size and not found:
+            if self.q[i][0] == item:
                 found = True
             else:
                 i += 1
         if not found:
             raise ValueError("Item doesn't exist.")
-        old_priority = self.__q[i][1]
-        self.__q[i] = (item, new_priority)
+        old_priority = self.q[i][1]
+        self.q[i] = (item, new_priority)
         if new_priority > old_priority:
-            while i > 0 and new_priority <= self.__q[(i - 1) // 2][1]:
-                self.__q[i], self.__q[(i - 1) // 2] = self.__q[(i - 1) // 2], self.__q[i]
+            while i > 0 and new_priority <= self.q[(i - 1) // 2][1]:
+                self.q[i], self.q[(i - 1) // 2] = self.q[(i - 1) // 2], self.q[i]
                 i = (i - 1) // 2
 
-    def isFull(self):
-        return self.__size == self.__max_size
+    def is_full(self):
+        return self.size == self.max_size
 
-    def isEmpty(self):
-        return self.__size == 0
+    def is_empty(self):
+        return self.size == 0
 
-    def showQueue(self):
+    def show_queue(self):
         """ prints str() representation of contents in descending priority order by mapping 'reverse' to each item. """
-        sorted_q = sorted(self.__q, key = lambda x: x[1], reverse = True)
+        sorted_q = sorted(self.q, key = lambda x: x[1], reverse = True)
         return " ".join(str(item) for item in sorted_q)
