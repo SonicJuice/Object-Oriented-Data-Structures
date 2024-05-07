5354b096ee2fff7dbf8ad3879e7bd275d3652fd1
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-05-07 20:05:24 +0100

    Update circular_queue.py

--- queues/circular_queue.py
+++ queues/circular_queue.py
@@ -1,37 +1,30 @@
 
- 
-class CircularQueue(object):
-    def __init__(self, qSize):
-        self.q = [None] * qSize
-        self.front = 0
-        self.rear = -1
-        self.size = 0
-        self.max_size = qSize
 
+class CircularQueue:
+    def __init__(self, capacity):
+        self.queue = [None for _ in range(capacity)]
+        self.front = 0
+        self.rear = 0
+        self.size = 0
+        self.capacity = capacity
+    
     def enqueue(self, item):
-        if self.is_full():
-            raise ValueError("Queue is full.")
-        else:
-            """ modulo operator allows pointers to wrap around to the beginning of the array once they reach the end. """
-            self.rear = (self.rear + 1) % self.max_size
-            self.q[self.rear] = item
-            self.size += 1
-            return True
+        if self.full():
+            raise ValueError("Queue is full")
+        self.queue[self.rear] = item
+        self.rear = (self.rear + 1) % self.capacity
+        self.size += 1
 
     def dequeue(self):
-        if self.is_empty():
-            raise ValueError("Queue is empty.")
-        else:
-            self.size -= 1
-            item = self.q[self.front]
-            self.front = (self.front + 1) % self.max_size
-            return item
-
-    def is_full(self):
-        return self.size == self.max_size
+        if self.empty():
+            raise ValueError("Queue is empty")
+        item = self.queue[self.front]
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return item
     
-    def is_empty(self):
-        return self.size == 0
+    def full(self):
+        return self.capacity == self.size
 
-    def show_queue(self):
-        return " ".join(str(item) for item in self.q[self.front:self.rear + 1])
+    def empty(self):
+        return self.size == 0
