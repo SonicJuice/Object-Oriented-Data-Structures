071f592e637ea537e0860d2ceac7d93311829d12
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-05-09 10:55:50 +0100

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,56 +1,19 @@
+class Stack(ThreadSafetyWrapper):
+    def __init__(self, maxsize=None):
+        self.stack = deque()
+        super().__init__(maxsize)
 
+    def push(self, item, block=True):
+        with self.protect_put(block):
+            self.stack.append(item)
 
-class Stack(object):
-    def __init__(self, max_size, iterable=False):
-        self.stack = []
-        self.size = 0
-        self.max_size = max_size
-        """ constructor method may take iterable, which specifies whether the stack should be initialised with items 
-        from an iterable. __stack is then intialised to represent the stack, as are the initial size __size, and the maximum 
-        size, self.max_size. """
-        if iterable:
-            for item in iterable:
-                self.push(item)
+    def pop(self, block=True):
+        with self.protect_get(block):
+            """ dequeue.pop() removes and returns the left-most item. """
+            return self.stack.pop()
 
-    def push(self,item):
-        """ adds a new item to the top of stack; returns False if stack is full. """
-        if self.isFull() == False:
-            """ .insert places a specified element at a specified list index. """
-            self.stack.insert(0, item)
-            self.size += 1
-            return True
-        else:
-            return False
+    def empty(self):
+        return len(self.stack) == 0
 
-    def pop(self):
-        """ returns top item from the stack. """
-        if self.isEmpty() == False:
-            self.size -= 1
-            """ .pop() removes an item from a list according to its index value.  """
-            return self.stack.pop(0)
-        else:
-            raise ValueError("Stack is empty.")
-
-    def peek(self):
-        """ returns the top item without removing it. """
-        if self.is_empty() == False:
-            self.size -= 1
-            return self.stack[0]
-        else:
-            raise ValueError("Stack is empty.")
-
-    def size(self):
-        """ returns no. of items in stack. """
-        return self.size
-
-    def is_full(self):
-        return self.size == self.max_size
-
-    def is_empty(self):
-        return self.size == 0
-  
-    def show_stack(self):
-        output = ""
-        for item in self.stack:
-            output += str(item) + "\n"
-        return output
+    def qsize(self):
+        return len(self.stack)
