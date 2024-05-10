8cb372d14ed946f4809d1ccf5ae4e14267fedf56
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-05-10 10:20:56 +0100

    Update circular_queue.py

--- queues/circular_queue.py
+++ queues/circular_queue.py
@@ -1,27 +1,30 @@
 class CircularQueue:
     def __init__(self, maxsize):
         self.queue = [None for _ in range(maxsize)]
-        """ current head and tail pointers, and the number of enqueued items. """
-        self.head = self.tail = self.size = 0
+        """ current front and rear pointers, and the number of enqueued items. """
+        self.front = 0
+        self.rear = -1
+        self.size = 0
         self.maxsize = maxsize
 
     def enqueue(self, item):
         if self.full():
             raise ValueError("Queue is full")
-        """ insert an item at the tail index, before incrementing it modulo the maxsize (to ensure 
+        """ insert an item at the rear index, before incrementing it modulo the maxsize (to ensure 
         that when the pointer wraps around to the beginning of the array upon reaching the end, 
         reusing the space released by dequeuing elements), as well as the size. """
-        self.queue[self.tail] = item
-        self.tail = (self.tail + 1) % self.maxsize
+        self.rear = (self.rear + 1) % self.maxsize
         self.size += 1
+        self.queue[self.rear] = item
 
     def dequeue(self):
         if self.empty():
             raise ValueError("Queue is empty")
-        """ remove and return the item at the head index, before incrementing it modulo the 
+        """ remove and return the item at the front index, before incrementing it modulo the 
         maxsize, as well as the size. """
-        item = self.queue[self.head]
-        self.head = (self.head + 1) % self.maxsize
+        item = self.queue[self.front]
+        self.queue[self.front] = None
+        self.front = (self.front + 1) % self.maxsize
         self.size -= 1
         return item
 
