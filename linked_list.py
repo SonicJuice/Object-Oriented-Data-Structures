75c82d50341afb3aebf1ce30b580dea854b743e4
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-05-13 22:17:19 +0100

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -7,7 +7,7 @@ class LinkedList:
     def __init__(self):
         self.head = self.tail = None
 
-    def append_left(self, data):
+    def add_head(self, data):
         new_node = ListNode(data)
         if not self.head:
             self.head = self.tail = new_node
@@ -15,7 +15,7 @@ class LinkedList:
             new_node.next = self.head
             self.head.prev = self.head = new_node
 
-    def append(self, data):
+    def add_tail(self, data):
         new_node = ListNode(data)
         if not self.head:
             self.head = self.tail = new_node
@@ -23,7 +23,41 @@ class LinkedList:
             new_node.prev = self.tail
             self.tail.next = self.tail = new_node
 
-    def pop_left(self):
+    def add_before(self, ref_node, data):
+        new_node = ListNode(data)
+        current = self.head
+        while current:
+            if current.data == ref_node:
+                if current == self.head:
+                    new_node.next = current
+                    current.prev = new_node
+                    self.head = new_node
+                else:
+                    new_node.prev = current.prev
+                    new_node.next = current
+                    current.prev.next = new_node
+                    current.prev = new_node
+                break
+            current = current.next
+
+    def add_after(self, ref_node, data):
+        new_node = ListNode(data)
+        current = self.head
+        while current:
+            if current.data == ref_node:
+                if current == self.tail:
+                    new_node.prev = current
+                    current.next = new_node
+                    self.tail = new_node
+                else:
+                    new_node.next = current.next
+                    new_node.prev = current
+                    current.next.prev = new_node
+                    current.next = new_node
+                break
+            current = current.next
+
+    def remove_head(self):
         if self.head:
             data = self.head.data
             if self.head == self.tail:
@@ -33,7 +67,7 @@ class LinkedList:
                 self.head.prev = None
             return data
 
-    def pop(self):
+    def remove_tail(self):
         if self.head:
             data = self.tail.data
             if self.head == self.tail:
@@ -42,6 +76,9 @@ class LinkedList:
                 self.tail = self.tail.prev
                 self.tail.next = None
             return data
+
+    def remove(self, data):
+        pass
 
     def traverse(self):
         current = self.head
