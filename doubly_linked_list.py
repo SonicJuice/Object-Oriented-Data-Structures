e573ffae8484b5fdcc2b0f49db276adf0bc41b77
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-05-14 11:54:08 +0100

    Update doubly_linked_list.py

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -7,78 +7,76 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = self.tail = None
 
-    def add_head(self, data):
+    def appendleft(self, data):
+        """ add a new node to the beginning of the list; if the list is empty, make it 
+        the head and tail. """
         new_node = ListNode(data)
         if not self.head:
             self.head = self.tail = new_node
         else:
+            """ set the new node's next to the current head. """
             new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
+            """ set the current head's previous to the new node. Finally, update the 
+            head to point to the new node """
+            self.head.prev = self.head = new_node
 
-    def add_tail(self, data):
+    def append(self, data):
+        """ add a new node to the end of the list. """
         new_node = ListNode(data)
         if not self.head:
             self.head = self.tail = new_node
         else:
             new_node.prev = self.tail
-            self.tail.next = new_node
-            self.tail = new_node
+            self.tail.next = self.tail = new_node
 
-    def add_before(self, ref_node, data):
-        new_node = ListNode(data)
+    def insert(self, data, pos):
+        if pos <= 0:
+            self.appendleft(data)
+        elif pos >= self.index(self.tail.data):
+            self.append(data)
+        else:
+            current = self.head
+            index = 0
+            while current and index < pos:
+                current = current.next
+                index += 1
+            new_node = ListNode(data)
+            new_node.prev, new_node.next = current.prev, current
+            current.prev.next = current.prev = new_node
+
+    def index(self, data):
         current = self.head
-        found = False
-        while current and not found:
-            if current.data == ref_node:
-                if current == self.head:
-                    new_node.next = current
-                    current.prev = new_node
-                    self.head = new_node
-                else:
-                    new_node.prev = current.prev
-                    new_node.next = current
-                    current.prev.next = new_node
-                    current.prev = new_node
-                found = True
+        index = 0
+        while current:
+            if current.data == data:
+                return index
             current = current.next
+            index += 1
 
-    def add_after(self, ref_node, data):
-        new_node = ListNode(data)
-        current = self.head
-        found = False
-        while current and not found:
-            if current.data == ref_node:
-                if current == self.tail:
-                    new_node.prev = current
-                    current.next = new_node
-                    self.tail = new_node
-                else:
-                    new_node.next = current.next
-                    new_node.prev = current
-                    current.next.prev = new_node
-                    current.next = new_node
-                found = True
-            current = current.next
-
-    def remove_head(self):
+    def popleft(self):
+        """ remove and return the node at the beginning of the list; if the list 
+        isn't empty, save the current head. """
         if self.head:
             data = self.head.data
+            """ if the head and tail are the same (there's only one node), set both to 
+            None. """
             if self.head == self.tail:
                 self.head = self.tail = None
             else:
-                self.head = self.head.next
-                self.head.prev = None
+                """ update the head to be the node following the current head. Then, 
+                set the prev pointer of the new head to None, as it's now the first 
+                node. """
+                self.head, self.head.prev = self.head.next, None
             return data
 
-    def remove_tail(self):
+    def pop(self):
+        """ remove and return the node at the end of the list. """
         if self.head:
             data = self.tail.data
             if self.head == self.tail:
                 self.head = self.tail = None
             else:
-                self.tail = self.tail.prev
-                self.tail.next = None
+                self.tail, self.tail.next = self.tail.prev, None
             return data
 
     def remove(self, data):
@@ -88,18 +86,10 @@ class DoublyLinkedList:
             while current and not found:
                 if current.data == data:
                     if current == self.head:
-                        self.remove_head()
+                        self.popleft()
                     elif current == self.tail:
-                        self.remove_tail()
+                        self.pop()
                     else:
-                        current.prev.next = current.next
-                        current.next.prev = current.prev
+                        current.prev.next, current.next.prev = current.next, current.prev
                     found = True
                 current = current.next
-
-    def traverse(self):
-        current = self.head
-        while current:
-            print(current.data, end=" ")
-            current = current.next
-        print()
