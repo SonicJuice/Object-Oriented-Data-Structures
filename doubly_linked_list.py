5c5d4e02e053f7efe04f257ca0265d1431a25c45
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-05-15 12:02:45 +0100

    Update doubly_linked_list.py

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -1,93 +1,211 @@
 class ListNode:
-    def __init__(self, data):
-        self.data = data
-        self.prev = self.next = None
+    def __init__(self, value):
+        self.value = value
+        self.next = self.prev = None
 
 class DoublyLinkedList:
-    def __init__(self):
+    def __init__(self, *values):
         self.head = self.tail = None
         self.size = 0
+        self.extend(values)
 
     def __len__(self):
         return self.size
 
+    def __str__(self):
+        return "None <= " + " <=> ".join(map(str, self)) + " => None"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __contains__(self, item):
+        node = self.head
+        while node:
+            if node.value == item:
+                return True
+            node = node.next
+        return False
+
+    def __eq__(self, other):
+        if type(other) is not type(self) or len(self) != len(other):
+            return False
+        node_self = self.head
+        node_other = other.head
+        while node_self and node_other:
+            if node_self.value != node_other.value:
+                return False
+            node_self = node_self.next
+            node_other = node_other.next
+        return True
+
+    def __validate_and_repair_index(self, index):
+        if not isinstance(index, int):
+            raise TypeError("List index must be an integer")
+        index = self.size + index if index < 0 else index
+        return index
+
     def __getitem__(self, index):
-        if index < 0:
-            index += len(self)
-        current = self.head
-        for _ in range(index):
-            current = current.next
-        return current.data
+        index = self.__validate_and_repair_index(index)
+        if 0 <= index < self.size:
+            i = 0
+            node = self.head
+            while node:
+                if i == index:
+                    return node.value
+                node = node.next
+                i += 1
+        raise IndexError("List index out of range")
 
-    def append(self, data):
-        new_node = ListNode(data)
-        if not self.head:
-            self.head = self.tail = new_node
+    def __setitem__(self, index, item):
+        index = self.__validate_and_repair_index(index)
+        if 0 <= index < self.size:
+            i = 0
+            node = self.head
+            while node:
+                if i == index:
+                    node.value = item
+                    return
+                node = node.next
+                i += 1
+        raise IndexError("List assignment index out of range")
+
+    def __delitem__(self, index):
+        index = self.__validate_and_repair_index(index)
+        if 0 < index < self.size - 1:
+            i = 0
+            node = self.head
+            while node:
+                if i == index:
+                    node.prev.next = node.next
+                    node.next.prev = node.prev
+                    self.size -= 1
+                    return
+                node = node.next
+                i += 1
+        elif index == 0 and self.head is not None:
+            self.head = self.head.next
+            if self.head:
+                self.head.prev = None
+            self.size -= 1
+        elif index == self.size - 1 and self.head is not None:
+            self.tail = self.tail.prev
+            if self.tail:
+                self.tail.next = None
+            self.size -= 1
         else:
-            new_node.prev = self.tail
-            self.tail.next = self.tail = new_node
-        self.size += 1
+            raise IndexError("List index out of range")
 
-    def appendleft(self, data):
-        new_node = ListNode(data)
+    def insertStart(self, item):
+        new_node = ListNode(item)
         if not self.head:
-            self.head = self.tail = new_node
+            self.head = new_node
+            self.tail = new_node
         else:
             new_node.next = self.head
-            self.head.prev = self.head = new_node
+            self.head.prev = new_node
+            self.head = new_node
         self.size += 1
 
-    def insert(self, index, data):
-        if index < 0:
-            index += len(self)
-        if index == 0:
-            self.appendleft(data)
-        elif index == len(self):
-            self.append(data)
+    def insertEnd(self, item):
+        new_node = ListNode(item)
+        if not self.tail:
+            self.tail = new_node
+            self.head = new_node
         else:
-            current = self.head
-            for _ in range(index):
-                current = current.next
-            new_node = ListNode(data)
-            new_node.prev, new_node.next = current.prev, current
-            if current.prev:
-                current.prev.next = new_node
-            current.prev = new_node
-            self.size += 1
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
 
-    def pop(self):
-        if self.tail:
-            data = self.tail.data
-            if self.head == self.tail:
-                self.head = self.tail = None
-            else:
-                self.tail, self.tail.next = self.tail.prev, None
-            self.size -= 1
-            return data
-    
-    def popleft(self):
-        if self.head:
-            data = self.head.data
-            if self.head == self.tail:
-                self.head = self.tail = None
-            else:
-                self.head, self.head.prev = self.head.next, None
-            self.size -= 1
-            return data
+    def insert(self, index, item):
+        index = self.__validate_and_repair_index(index)
+        if index > self.size - 1:
+            self.insertEnd(item)
+        elif index <= 0:
+            self.insertStart(item)
+        else:
+            i = 0
+            node = self.head
+            while node:
+                if i == index - 1:
+                    new_node = ListNode(item)
+                    new_node.next = node.next
+                    new_node.prev = node
+                    node.next = new_node
+                    if new_node.next:
+                        new_node.next.prev = new_node
+                    self.size += 1
+                    return
+                node = node.next
+                i += 1
 
-    def remove(self, data):
-        current = self.head
-        found = False
-        while current and not found:
-            if current.data == data:
-                if current.prev:
-                    current.prev.next = current.next
-                else:
-                    self.head = current.next
-                if current.next:
-                    current.next.prev = current.prev
-                else:
-                    self.tail = current.prev
-                self.size -= 1
-                found = True
-            current = current.next
+    def extend(self, seq=()):
+        for i in seq:
+            self.insertEnd(i)
+
+    def remove(self, item):
+        if not self.head:
+            raise IndexError("List is empty")
+        else:
+            if self.head.value == item:
+                self.head = self.head.next
+                if self.head:
+                    self.head.prev = None
+            elif self.tail.value == item:
+                self.tail = self.tail.prev
+                if self.tail:
+                    self.tail.next = None
+            else:
+                node = self.head
+                try:
+                    while node:
+                        if node.value == item:
+                            node.prev.next = node.next
+                            if node.next:
+                                node.next.prev = node.prev
+                            self.size -= 1
+                            return
+                        node = node.next
+                except AttributeError:
+                    raise ValueError("Value not present in list") from None
+            self.size -= 1
+
+    def pop(self, index=-1):
+        if self.size == 0:
+            raise IndexError("List is empty")
+        t = type(index)
+        if t is not int:
+            raise TypeError("{} can't be interpreted as an integer".format(t))
+        index = self.__validate_and_repair_index(index)
+        if index == self.size - 1:
+            item = self.tail.value
+            del self[self.size - 1]
+            return item
+        if index == 0:
+            item = self.head.value
+            del self[0]
+            return item
+        i = 0
+        node = self.head
+        while node:
+            if i == index:
+                item = node.value
+                del self[index]
+                return item
+            node = node.next
+            i += 1
+
+    def index(self, item):
+        for index, el in enumerate(self):
+            if el == item:
+                return index
+        return -1
+
+    def count(self, item):
+        count = 0
+        node = self.head
+        while node:
+            if node.value == item:
+                count += 1
+            node = node.next
+        return count
