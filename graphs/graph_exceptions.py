6c73f4bff7a1c256065ae43fcc52ed18ab2e99ef
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-06-04 11:01:44 +0100

    Update graph_exceptions.py

--- graphs/graph_exceptions.py
+++ graphs/graph_exceptions.py
@@ -1,8 +1,7 @@
 class NodeNotFoundError(Exception):
+    """ raised when a node doesn't exist. """
     pass
 
 class DuplicateKeyError(Exception):
-    pass
-
-class InvalidRotationError(Exception):
+    """ raised when a key already exists. """
     pass
