a7e824929190b64e10e09ea950997a4ff12474d6
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-06-06 15:01:21 +0100

    Update murmurhash3.py

--- hash_table/murmurhash3.py
+++ hash_table/murmurhash3.py
@@ -1,47 +1,45 @@
-""" a non-cryptographic hash function suitable for general hash-based lookup. It's designed 
-to provide a good distribution of hashes for input keys, making it highly effective 
-for hash tables. """
-def murmurhash3(key, seed=0x0, signed=True):
+""" a non-cryptographic hash function suitable for general hash-based lookup. """
+def murmurhash3(key, seed=0x0):
     """ bytearray returns returns mutable sequence of integers in the range 0 <= x < 256. 
-    str.encode() converts a string into a collection of bytes, using a given encoding 
-    (utf-8 by default). """
+    str.encode() converts a string into a collection of bytes via utf-8. """
     key_bytes = bytearray(key.encode())
     key_length = len(key_bytes)
     """ number of 4 byte blocks in the key. """
     nblocks = key_length // 4
     """ initial seed value ensures that the resulting hash will be different for similar 
-    inputs. This is especially useful in hash tables, as it allows for the even distribution 
-    of keys across buckets. """
+    inputs. This is especially useful in hash tables, as it allows for even key 
+    distribution across buckets. """
     h1 = seed
-    """ c1 and c2 are large enough to prevent aliasing (repetitive patterns) in hashes 
-    without significant overhead. They're also coprime, ensuring that the hashing 
-    process isn't biased towards certain patterns in the keys. Finally, they're 
-    sufficiently different enough to contribute to the avalanche effect, 
-    where small changes in the key significantly changes the hash. """
+    """ c1, c2, and 0xe6546b64 are derived from primes, helping to uniformly spread the 
+    input bits. They also provide a mixture of high and low bit patterns, maximising 
+    entropy (unpredictability) during mixing (thoroughly combining and transforming the 
+    input data to produce a hash). Furthermore, the mixing strength of the specific 
+    values avoid patterns that would reduce the effectiveness of bitwise operations. """
     c1 = 0xcc9e2d51
     c2 = 0x1b873593
 
     for block_start in range(0, nblocks * 4, 4):
-        """ construct a 32-bit integer from the four bytes of the current block. """
+        """ combine each 4-byte block into a 32-bit integer. """
         k1 = key_bytes[block_start + 3] << 24 | \
              key_bytes[block_start + 2] << 16 | \
              key_bytes[block_start + 1] <<  8 | \
              key_bytes[block_start + 0]
 
+        """ bitwise AND with 0xFFFFFFFF avoids overflow by ensuring that intermediate 
+        results are constrained to 32 bits. """
         k1 = (c1 * k1) & 0xFFFFFFFF
+        """ 13, 15, 17, 19 are chosen to be non-powers-of-two to avoid alignment issues
+        and ensure that all bits influence each other. """
         k1 = (k1 << 15 | k1 >> 17) & 0xFFFFFFFF
         k1 = (c2 * k1) & 0xFFFFFFFF
 
         h1 ^= k1
         h1 = (h1 << 13 | h1 >> 19) & 0xFFFFFFFF
-        """ 5 is small enough to contribute a uniform distribution of hashes, whilst 
-        0xe6546b64 is large enough to effectively aid in bit mixing (). """
         h1 = (h1 * 5 + 0xe6546b64) & 0xFFFFFFFF
 
-    """ calculate the starting index of the remaining bytes. """
+    """ combine remaining 1 to 3 bytes not covered by the 4-byte blocks. """
     tail_index = nblocks * 4
     k1 = 0
-    """ determine the number of remaining bytes (0, 1, 2, or 3). """
     tail_size = key_length & 3
 
     if tail_size >= 3:
@@ -57,24 +55,12 @@ def murmurhash3(key, seed=0x0, signed=True):
         k1 = (k1 * c2) & 0xFFFFFFFF
         h1 ^= k1
 
-    final_hash = _finalise(h1 ^ key_length)
-
-    if signed:
-        """ if the MSB is 0. """
-        if final_hash & 0x80000000 == 0:
-            return final_hash
-        else:
-            """ convert from unsigned to signed integer via two's compliment. """
-            return -((final_hash ^ 0xFFFFFFFF) + 1)
-    else:
-        return final_hash
-
+    return _finalise(h1 ^ key_length)
+    
 def _finalise(hash):
     hash ^= hash >> 16
-    """ . """
     hash= (hash * 0x85ebca6b) & 0xFFFFFFFF
     hash^= hash >> 13
-    """ . """
     hash= (hash * 0xc2b2ae35) & 0xFFFFFFFF
     hash^= hash >> 16
     return hash
