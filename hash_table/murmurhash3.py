e7f3ec4c15fd17c47c10c4be1dfdcf37c1e17869
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-07-13 23:22:51 +0100

    Update murmurhash3.py

--- hash_table/murmurhash3.py
+++ hash_table/murmurhash3.py
@@ -1,7 +1,8 @@
 """ a non-cryptographic hash function suitable for general hash-based lookup. """
 def murmurhash3(key, seed=0x0):
-    """ bytearray returns returns mutable sequence of integers in the range 0 <= x < 256. 
-    str.encode() converts a string into a collection of bytes via utf-8. """
+    """ bytearray returns returns mutable sequence of integers in the range 
+    0 <= x < 256. str.encode() converts a string into a collection of bytes 
+    via utf-8. """
     key_bytes = bytearray(key.encode())
     key_length = len(key_bytes)
     """ number of 4 byte blocks in the key. """
@@ -10,9 +11,9 @@ def murmurhash3(key, seed=0x0):
     inputs. This is especially useful in hash tables, as it allows for even key 
     distribution across buckets. """
     h1 = seed
-    """ c1, c2, and 0xe6546b64 are derived from primes, helping to uniformly spread the 
-    input bits. They also provide a mixture of high and low bit patterns, maximising 
-    entropy during mixing. Furthermore, the mixing strength of the specific values avoid 
+    """ c1, c2, and 0xe6546b64 are derived from primes, helping to uniformly spread 
+    input bits. They also represent high and low bit patterns, maximising entropy 
+    during mixing. Furthermore, the mixing strength of the specific values avoid 
     patterns that would reduce the effectiveness of bitwise operations. """
     c1 = 0xcc9e2d51
     c2 = 0x1b873593
@@ -55,7 +56,7 @@ def murmurhash3(key, seed=0x0):
         h1 ^= k1
 
     return _finalise(h1 ^ key_length)
-    
+
 def _finalise(hash):
     hash ^= hash >> 16
     hash= (hash * 0x85ebca6b) & 0xFFFFFFFF
