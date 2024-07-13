d92619c792605a7494bfcb19b41c58dacd765344
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-07-13 22:57:21 +0100

    Create stack.py

---
+++ queues/stack.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+""" a queue derivation in which elements must join and leave via the top. """
+class Stack(ThreadSafetyWrapper):
+    def __init__(self, maxsize=None):
+        self.stack = deque()
+        super().__init__(maxsize)
+
+    def push(self, item, block=True):
+        with self.protect_put(block):
+            self.stack.append(item)
+
+    def pop(self, block=True):
+        with self.protect_get(block):
+            """ dequeue.pop() removes and returns the left-most item. """
+            return self.stack.pop()
+            
+    def empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
